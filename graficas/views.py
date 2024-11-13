c6f9a4ececeaf15789518e881564f0f35dd042b1
AngeluisArroyo <arroyo.luis3d@gmail.com>
2024-11-13 13:50:49 -0600

    feat: :hammer: Logicas_Graficas

--- graficas/views.py
+++ graficas/views.py
@@ -16,3 +16,35 @@ def grafica_view(request):
         'etiquetas':etiquetas,
         'datos':datos
     })
+def grafica_view_2(request):
+    ''' 
+    datos para la grafica 
+    
+    Args:
+    requests (_type_): _descripcion_
+    ''' 
+    
+    etiquetas = ['Enero','Frebrero','Marzo','Abril','Mayo']
+    
+    datos = [5,15,10,20,25]
+    
+    return render (request,'graficas/graficas_2.html', {
+        'etiquetas':etiquetas,
+        'datos':datos
+    })
+def grafica_view_3(request):
+    ''' 
+    datos para la grafica 
+    
+    Args:
+    requests (_type_): _descripcion_
+    ''' 
+    
+    etiquetas = ['Enero','Frebrero','Marzo','Abril','Mayo']
+    
+    datos = [50,150,100,200,300]
+    
+    return render (request,'graficas/graficas_3.html', {
+        'etiquetas':etiquetas,
+        'datos':datos
+    })
